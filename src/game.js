1d19c174538ad14bf04be0b778494b9ec3cd6145
Victor <jvmxgs@gmail.com>
2024-05-25 20:51:40 -0600

    migrated to vite

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
-import MenuScene from './scenes/menuScene.js'
 import Boot from './scenes/boot.js'
+import MenuScene from './scenes/menuScene.js'
 import config from './util/config'
 
 config.scene = [Boot, MenuScene]
